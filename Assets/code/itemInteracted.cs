17f63e89ca464aec418ccf21de3b2587816a294d
xxItzBoscoxx <res556@nyu.edu>
2023-03-06 17:18:57 -0500

    Clicks Working

---
+++ Assets/code/itemInteracted.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class itemInteracted : MonoBehaviour
+{
+    public bool itemClicked;
+    public string itemName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemClicked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseOver(){
+        Debug.Log("here");
+        if (Input.GetMouseButtonDown(0)) {
+            itemName = gameObject.tag;
+        }
+    }
+}
