66fd9a75f60c5d3e31931ebd8110201472cfedaa
xxItzBoscoxx <res556@nyu.edu>
2023-03-08 16:11:46 -0500

    TestTriggers

--- Assets/code/DialogueTrigger.cs
+++ Assets/code/DialogueTrigger.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour
 {
 
     public Dialogue dialogue;
+    public int itemsClicked;
+    public Button humanButton;
+
+    public void Start(){
+        itemsClicked = 0;
+    }
+
+    public void Update(){
+        if(itemsClicked == 4){
+            Debug.Log("yay");
+            humanButton.interactable = true;
+            Debug.Log(humanButton.IsInteractable());
+        }
+    }
 
     public void TriggerDialogue(){
+        itemsClicked += 1;
+        Debug.Log(itemsClicked);
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
