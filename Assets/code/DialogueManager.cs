f8ab9108d8e913d780cc2ab80393596205877891
xxItzBoscoxx <res556@nyu.edu>
2023-03-08 09:10:55 -0500

    Test Dialogue V2

--- Assets/code/DialogueManager.cs
+++ Assets/code/DialogueManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
     private Queue<string> sentences;
     // Start is called before the first frame update
     void Start()
@@ -12,7 +15,8 @@ public class DialogueManager : MonoBehaviour
     }
 
     public void StartDialogue(Dialogue dialogue){
-        Debug.Log("Starting conversation with " + dialogue.name);
+
+        nameText.text = dialogue.name;
 
         sentences.Clear();
 
@@ -30,7 +34,17 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
-        Debug.Log(sentence);
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence){
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray()){
+            dialogueText.text += letter;
+            yield return null;
+        }
+
     }
 
     void EndDialogue(){
